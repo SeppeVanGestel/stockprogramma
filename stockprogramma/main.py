8e593fe5dd0b11bf82e58ff2cd8c9dd8b50fffcd
weetnix <seppevangestel53@gmail.com>
2022-04-25 08:28:23 +0200

    aanpassing van website naar api

--- stockprogramma/main.py
+++ stockprogramma/main.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template
 from flask_restful import Api
-from blueprint import simple_page
+
 
 from resources.route import initialize_routes
 from database.db import initialize_db
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+import os
+from dotenv import load_dotenv
 
+if os.environ.get("JWT_SECRET_KEY") is None:
+    load_dotenv()
 
 app = Flask(__name__)
-app.config["JWT_SECRET_KEY"] = "qrstock" # change this!
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY') # change this!
 
 api = Api(app)
 
 app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.pgmzz.mongodb.net/Stockbeheer?retryWrites=true&w=majority' # Stockbeheer is naam van de db
+    'host': os.environ.get('uri') # Stockbeheer is naam van de db
 }
 
 bcrypt = Bcrypt(app)
@@ -23,7 +27,7 @@ jwt = JWTManager(app)
 initialize_db(app)
 initialize_routes(api)
 
-app.register_blueprint(simple_page, url_prefix="/log")
+
 
 
 if __name__== "__main__":
--- stockprogramma/resources/route.py
+++ stockprogramma/resources/route.py
@@ -6,7 +6,7 @@ from stockprogramma.resources.auth import SignupUi, LoginUi
 
 
 def initialize_routes(api): 
-    api.add_resource(SignupUi, '/api/auth/signupui') 
-    api.add_resource(LoginUi, '/api/auth/loginui') # dit is de response, de request is log/login uit de blueprint, staat ook in het form
+    api.add_resource(SignupUi, '/api/auth/signup') 
+    api.add_resource(LoginUi, '/api/auth/login') # dit is de response, de request is log/login uit de blueprint, staat ook in het form
     api.add_resource(UsersApi, '/api/users')
     api.add_resource(ProductsApi, '/api/products')
